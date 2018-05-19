640665f295b4fd35a9cefbcf9fe84bd0e145f8b0
fanjunwei <fanjunwei003@163.com>
2018-05-19 19:21:36 +0800

    log write

--- log_server/main.go
+++ log_server/main.go
@@ -5,6 +5,14 @@ import (
 	"log"
 	"encoding/json"
 	"fmt"
+	"os"
+	"path/filepath"
+)
+
+var (
+	logChannel  = make(chan LogReq, 100)
+	exitChannel = make(chan int)
+	fileHandler = make(map[string]*os.File)
 )
 
 type LogReq struct {
@@ -16,12 +24,18 @@ type LogRes struct {
 	Success bool `json:"success"`
 }
 
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
+func httpWriteLog(w http.ResponseWriter, r *http.Request) {
 	var logReq LogReq
 	var buffer []byte;
 	if r.Method == "POST" {
 		r.Body.Read(buffer)
-		json.Unmarshal(buffer, &logReq)
+		err := json.Unmarshal(buffer, &logReq)
+		if err != nil {
+			w.Write([]byte(fmt.Sprintf("req error:", err)))
+			w.WriteHeader(400)
+			return
+		}
+		logChannel <- logReq
 		res := LogRes{
 			Success: true,
 		}
@@ -38,10 +52,61 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		w.Write([]byte("Log Server OK"))
+	} else {
+		w.Write([]byte("Method Not Allowed"))
+		w.WriteHeader(405)
+	}
+}
+func PathExists(path string) (bool) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	} else {
+		return false
+	}
+
+}
+
+func getLogFile(logFilePath string) (file *os.File, err error) {
+	dir := filepath.Dir(logFilePath)
+	file, ok := fileHandler[logFilePath]
+	if ok {
+		return file, nil
+	}
+	if !PathExists(dir) {
+		os.MkdirAll(dir, 0777)
+	}
+	file, err = os.OpenFile(logFilePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	if err == nil {
+		fileHandler[logFilePath] = file
+	}
+	return
+}
+func writeLog() {
+
+	for {
+		select {
+		case <-exitChannel:
+			return
+		case logData := <-logChannel:
+			fd, err := getLogFile(logData.FileName)
+			if err != nil {
+				fmt.Println(err)
+			}
+			fd.Write([]byte(logData.Text))
+		}
+	}
+}
 func main() {
-	http.HandleFunc("/", sayhelloName)                 //设置访问的路由
-	err := http.ListenAndServe("127.0.0.1:35678", nil) //设置监听的端口
+	go writeLog()
+	http.HandleFunc("/", home)
+	http.HandleFunc("/log", httpWriteLog)
+	err := http.ListenAndServe("127.0.0.1:35678", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+	exitChannel <- 0
 }
