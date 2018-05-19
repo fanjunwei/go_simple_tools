100f4241a9ff7e55ebbf6e685061ee78c10efe07
fanjunwei <fanjunwei003@163.com>
2018-05-19 18:01:00 +0800

    http server

--- log_server/main.go
+++ log_server/main.go
@@ -9,7 +9,7 @@ import (
 
 type LogReq struct {
 	FileName string `json:"file_name"`
-	Text string `json:"text"`
+	Text     string `json:"text"`
 }
 
 type LogRes struct {
@@ -19,27 +19,27 @@ type LogRes struct {
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	var logReq LogReq
 	var buffer []byte;
-	if r.Method=="POST"{
+	if r.Method == "POST" {
 		r.Body.Read(buffer)
-		json.Unmarshal(buffer,&logReq)
-		res:=LogRes{
-			Success:true,
+		json.Unmarshal(buffer, &logReq)
+		res := LogRes{
+			Success: true,
 		}
-		out,err := json.Marshal(res)
-		if err!=nil{
+		out, err := json.Marshal(res)
+		if err != nil {
 			w.Write([]byte(fmt.Sprint(err)))
 			w.WriteHeader(500)
-		}else {
+		} else {
 			w.Write(out)
 		}
-	}else {
+	} else {
 		w.Write([]byte("Method Not Allowed"))
 		w.WriteHeader(405)
 	}
 
 }
 func main() {
-	http.HandleFunc("/", sayhelloName)       //设置访问的路由
+	http.HandleFunc("/", sayhelloName)                 //设置访问的路由
 	err := http.ListenAndServe("127.0.0.1:35678", nil) //设置监听的端口
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
