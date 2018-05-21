df44d38fb608164da844fd964621632fc96c60b4
fanjunwei <fanjunwei003@163.com>
2018-05-21 10:45:39 +0800

    网卡配合使用模板

--- centos_set_network/main.go
+++ centos_set_network/main.go
@@ -7,12 +7,25 @@ import (
 	"fmt"
 	"path/filepath"
 	"strings"
+	"html/template"
 )
 
 type NetWorkInfo struct {
 	Mac     string `json:"mac"`
 	Ip      string `json:"ip"`
 	Gateway string `json:"gateway"`
+}
+
+func tpl_write_file(fd *os.File, tpl string, data interface{}) {
+	t := template.New("")
+	t, err := t.Parse(tpl)
+	if err != nil {
+		panic("Error : " + err.Error())
+	}
+	err = t.Execute(fd, data)
+	if err != nil {
+		panic("Error : " + err.Error())
+	}
 }
 
 func main() {
@@ -31,45 +44,51 @@ func main() {
 		for _, item := range netInfos {
 			ifname, ok := macNameMap[strings.ToLower(strings.Replace(item.Mac, "-", ":", -1))]
 			if ok {
-				var value string
-				if item.Ip != "" {
-					tmpl := `TYPE=Ethernet
-BOOTPROTO=static
+
+				tpl := `
+TYPE=Ethernet
 DEFROUTE=yes
 PEERDNS=yes
 PEERROUTES=yes
 IPV4_FAILURE_FATAL=no
-NAME=%s
-DEVICE=%s
+NAME={{.ifname}}
+DEVICE={{.ifname}}
 ONBOOT=yes
-IPADDR=%s
-NETMASK=%s
-GATEWAY=%s
-HWADDR=%s
-ZONE=public`
+HWADDR={{.mac}}
+ZONE=public
+{{if .static}}
+BOOTPROTO=static
+IPADDR={{.ip}}
+NETMASK={{.netmask}}
+{{if ne .gateway "" }}GATEWAY={{.gateway}}{{end}}
+{{else}}
+BOOTPROTO=dhcp
+{{end}}
+`
+				configPath := filepath.Join(outdir, fmt.Sprintf("ifcfg-%s", ifname))
+				fd, err := os.OpenFile(configPath, os.O_WRONLY|os.O_CREATE, 0644)
+				if err != nil {
+					panic("Error : " + err.Error())
+				}
+				fd.Truncate(0)
+				data := make(map[string]interface{})
+				data["ifname"] = ifname
+				data["mac"] = strings.ToUpper(item.Mac)
+				if item.Ip != "" {
+
 					ip, sub_net, err := net.ParseCIDR(item.Ip)
 					if err != nil {
 						panic("Error : " + err.Error())
 					}
-					value = fmt.Sprintf(tmpl, ifname, ifname, ip.String(), net.IP(sub_net.Mask).String(), item.Gateway, strings.ToUpper(item.Mac))
+					data["ip"] = ip.String()
+					data["netmask"] = net.IP(sub_net.Mask).String()
+					data["gateway"] = item.Gateway
+					data["static"] = true
 				} else {
-					tmpl := `TYPE=Ethernet
-BOOTPROTO=dhcp
-DEFROUTE=yes
-PEERDNS=yes
-PEERROUTES=yes
-IPV4_FAILURE_FATAL=no
-NAME=%s
-DEVICE=%s
-ONBOOT=yes
-HWADDR=%s
-ZONE=public`
-					value = fmt.Sprintf(tmpl, ifname, ifname, strings.ToUpper(item.Mac))
+					data["static"] = false
 				}
-				configPath := filepath.Join(outdir, fmt.Sprintf("ifcfg-%s", ifname))
-				fd, _ := os.OpenFile(configPath, os.O_WRONLY|os.O_CREATE, 0644)
-				fd.Truncate(0)
-				fd.Write([]byte(value))
+				tpl_write_file(fd, tpl, data)
+
 			}
 
 		}
