b2b2cdd7e2df8c3980a7d2ab3e434177ed031e94
fanjunwei <fanjunwei@t2cloud.net>
2022-08-16 12:55:16 +0800

    file server

--- file_server/main.go
+++ file_server/main.go
@@ -1,20 +1,49 @@
 package main
 
 import (
-	"os"
-	"net/http"
+	"fmt"
 	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-	if len(os.Args) >= 3 {
-		addr := os.Args[1]
-		filePaht := os.Args[2]
-		http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(filePaht))))
-		err := http.ListenAndServe(addr, nil)
-		if err != nil {
-			log.Fatal("ListenAndServe: ", err)
+	filePath := "."
+	port := "7000"
+	addr := "0.0.0.0"
+	tag := ""
+	if len(os.Args) >= 2 {
+		for index := range os.Args {
+			data := os.Args[index]
+			if tag == "" {
+				if data == "-a" {
+					tag = "a"
+				} else if data == "-p" {
+					tag = "p"
+				} else if data == "-d" {
+					tag = "d"
+				} else if data == "-h" {
+					fmt.Printf(`usage: %s [OPTIONS]
+    -a ADDR: address (default: 0.0.0.0)
+    -p PORT: port number (default: 7000)
+    -d DIR:  root directory (default: public)`, os.Args[0])
+					os.Exit(1)
+				}
+			} else if tag == "a" {
+				addr = data
+			} else if tag == "p" {
+				port = data
+			} else if tag == "d" {
+				filePath = data
+			}
 		}
+	}
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(filePath))))
+	fmt.Printf("listen: %s:%s\n", addr, port)
+	fmt.Printf("path: %s\n", filePath)
+	err := http.ListenAndServe(fmt.Sprintf("%s:%s", addr, port), nil)
+	if err != nil {
+		log.Fatal("ERROR: ", err)
 	}
 
 }
